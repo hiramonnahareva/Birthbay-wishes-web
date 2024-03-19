cbae51683705c04bd8770c3bac9d1e1462056e61
Coderflixdev <devhiramon.e@gmail.com>
2024-03-19 22:26:40 +0600

    Deploy to GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,35 @@
-import { useState } from 'react'
-import './App.css'
-import Confetti from 'react-confetti'
-import img from './assets/Animation - 1710828363366.gif'
-import img1 from './assets/happy-birthday-gif-45.gif'
+// import { useState } from 'react'
+// import './App.css'
+// import Confetti from 'react-confetti'
+// import img from './assets/Animation - 1710828363366.gif'
+// import img1 from './assets/happy-birthday-gif-45.gif'
 
 function App() {
-  const [click, setClick] = useState(false)
+  // const [click, setClick] = useState(false)
 
   return (
  
-       <div className='flex justify-center h-[100vh] items-center'>
-            <div className={`${click && 'hidden'}`} onClick={() => setClick(!click)}>
+      //  <div className='flex justify-center h-[100vh] items-center'>
+      //       <div className={`${click && 'hidden'}`} onClick={() => setClick(!click)}>
 
-                <img className='w-[300px]' src={img} alt="" />
-                <p className='text-center animate-bounce text-[#5050d0]'>Click Me</p>
-            </div>
+      //           <img className='w-[300px]' src={img} alt="" />
+      //           <p className='text-center animate-bounce text-[#5050d0]'>Click Me</p>
+      //       </div>
 
-            {
-                click && 
-                <div>
-                    <Confetti className='[w-100vw]' />
-                    <img className='' src={img1} alt="" />
-                    <p className='text-center font-bold text-clip text-8xl text-[#ffa5cf] font-outline-2 outline-blue-800 uppercase'>Tinni</p>
-                </div>
+      //       {
+      //           click && 
+      //           <div>
+      //               <Confetti className='[w-100vw]' />
+      //               <img className='' src={img1} alt="" />
+      //               <p className='text-center font-bold text-clip text-8xl text-[#ffa5cf] font-outline-2 outline-blue-800 uppercase'>Tinni</p>
+      //           </div>
 
-            }
+      //       }
 
-        </div>
+      //   </div>
+      <div>
+        <h2>Hello World</h2>
+      </div>
   )
 }
 
